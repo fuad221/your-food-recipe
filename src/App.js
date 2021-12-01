393f43e5e3e62d4247fa42d50719b5088e0e6dee
fuad221 <fuad221@outlook.com>
2021-12-01 18:19:52 +0100

    I created the Interface page with out commands

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
+import { useState } from "react";
+
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState("")
   return (
     <div>
-test
+      <h1>
+        <u> Your food recipe </u>🍰🍠
+      </h1>
+      <form className="app__searchForm">
+        <input
+          type="text"
+          placeholder="Type the Ingredient"
+          autoComplete="Off"
+          className="app__input"
+        />
+        <select className="app__healthLabels">
+          <option
+            value="vegan"
+
+    
+          >
+            vegan
+          </option>
+          <option
+            value="vegetarian"
+
+     
+          >
+            vegetarian
+          </option>
+          <option
+            value="low-sugar"
+
+          >
+            low-sugar
+          </option>
+          <option
+            value="dairy-free"
+
+          >
+            dairy-free
+          </option>
+          <option
+            value=" immuno-supportive "
+
+          >
+            immuno-supportive
+          </option>
+          <option
+            value="wheat-free"
+
+          >
+            wheat-free
+          </option>
+        </select>
+
+<button type="submit" className="app__submit">Get Recipe</button>
+      </form>
     </div>
   );
 }
